d3bd90c82edae212ca8400020f0aeb7086456a96
hotwater <vlad.dalechin@gmail.com>
2024-04-27 17:09:54 +0300

    feat: dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,6 +4,8 @@ import { Component, useEffect, useState } from "react";
 import { CovalentClient } from "@covalenthq/client-sdk";
 import Image from "next/image";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
 
 // class TableFromArray extends Component {
 //     renderTableData() {
@@ -117,9 +119,9 @@ export default function Dashboard() {
 
   return (
     <div className="bg-black min-h-screen mx-auto">
-      <div className="mt-16 p-8 rounded-md ml-32 mr-32 border-gray-400 border-2 justify-between flex">
-        <div>
-          <input type="text" placeholder="Wallet adress..."
+      <div className="mt-16 p-8 rounded-lg  border-gray-400 border-2 justify-between flex">
+        <div className="flex">
+          <Input type="text" placeholder="Wallet adress..."
             value={walletAddress}
             onChange={handleChange}
             className="border rounded-md p-2 ml-2" />
@@ -135,37 +137,40 @@ export default function Dashboard() {
           <div className="text-6xl items-center">{walletAddressSubmit}</div>
         </div>}
       </div>
-      {tokens.length > 0 && <div className="overflow-x-auto mt-16 ml-32 mr-32">
-        <table className="min-w-full text-white border-collapse border border-gray-300">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Token</th>
-              <th className="border border-gray-300 px-4 py-2">Chain</th>
-              <th className="border border-gray-300 px-4 py-2">Price</th>
-              <th className="border border-gray-300 px-4 py-2">Balance</th>
-              <th className="border border-gray-300 px-4 py-2">Value</th>
-            </tr>
-          </thead>
-          <tbody>
+      {tokens.length > 0 &&
+        <>
+          <div className="w-max  rounded-tl-lg rounded-tr-lg mt-12 px-12 py-4 text-black bg-gray-400">
+            Holdings
+          </div>
+          <div className="grid grid-cols-5 items-center rounded-tr-lg rounded-bl-lg rounded-br-lg border border-gray-400">
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Token</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b ">Chain</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Price</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b ">Balance</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Value</span>
+
             {tokens.map((item, index) => (
-              <tr key={index}>
-                <td className="border text-center text-white px-4 py-2">{item.contract_ticker_symbol}</td>
-                <td className="border content-center justify-items-center justify-center px-4 py-2">
+              <>
+                <div>
+                  <p className="text-muted-foreground px-4 pt-2 text-sm">{item.contract_ticker_symbol}</p>
+                  <p className="text-white px-4 text-xl pb-2">{item.contract_display_name}</p>
+                </div>
+                <span className="content-center flex justify-center px-4 py-2">
                   <Link className="" target="_blank"
                     href={`https://scrollscan.com/address/${item.contract_address}`}>
                     <Image src="/static/scroll.svg" className="" width={30} height={30} />
                   </Link>
-                </td>
-                <td className="border text-center px-4 py-2">${item.quote_rate}</td>
-                <td className="border text-center px-4 py-2">
-                  {parseFloat(item.pretty_quote.replace("$", "")) / item.quote_rate} {item.contract_ticker_symbol}
-                </td>
-                <td className="border text-center px-4 py-2">{item.pretty_quote}</td>
-              </tr>
+                </span>
+                <span className="text-center px-4 py-2">${item.quote_rate}</span>
+                <span className="text-center px-4 py-2">
+                  {(parseFloat(item.pretty_quote.replace("$", "")) / item.quote_rate).toFixed(4)} {item.contract_ticker_symbol}
+                </span>
+                <span className="text-center px-4 py-2">{item.pretty_quote}</span>
+              </>
             ))}
-          </tbody>
-        </table>
-      </div>}
+          </div>
+        </>
+      }
 
       {/*{images.length > 0 && <div>*/}
       {/*    <table className="min-w-full text-white border-collapse border border-gray-300">*/}
@@ -184,50 +189,54 @@ export default function Dashboard() {
       {/*    </table>*/}
       {/*</div>}*/}
 
-      {transactions.length > 0 && <div className="overflow-x-auto mt-16 ml-32 mr-32">
-        <table className="min-w-full text-white border-collapse border border-gray-300">
-          <thead>
-            <tr>
-              <th className="border-t border-b border-gray-300 text-gray-400 px-4 py-2">Hash</th>
-              <th className="border-t border-b border-gray-300 text-gray-400 px-4 py-2">From</th>
-              <th className="border-t border-b border-gray-300 text-gray-400 px-4 py-2">Out</th>
-              <th className="border-t border-b border-gray-300 text-gray-400 px-4 py-2">In/Out</th>
-              <th className="border-t border-b border-gray-300 text-gray-400 px-4 py-2">Value</th>
-            </tr>
-          </thead>
-          <tbody>
+
+      {transactions.length > 0 &&
+        <>
+          <div className="w-max  rounded-tl-lg rounded-tr-lg mt-12 px-12 py-4 text-black bg-gray-400">
+            Recent Activity
+          </div>
+          <div className="grid grid-cols-5 items-center rounded-tr-lg  border border-gray-400">
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Hash</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b ">From</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Out</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b ">In/Out</span>
+            <span className="px-4 py-7 text-muted-foreground text-center border-gray-400 border-b">Value</span>
+
             {transactions.slice(0, showAll ? transactions.length : 5).map((item, index) => (
-              <tr key={index}>
-                <td className="border-t border-b border-gray-300 text-center text-gray-400 px-4 py-2">
-                  <Link target="_blank" href={`https://scrollscan.com/tx/${item.tx_hash}`}>
-                    {item.tx_hash.substring(0, 4) + '...' + item.tx_hash.substring(item.tx_hash.length - 4)}
-                  </Link>
-                </td>
-                <td className="border-t border-b border-gray-300 text-center text-white px-4 py-2">
+              <>
+                <div>
+
+                  <Link className="text-center px-4 pt-2 text-gray-400" target="_blank" href={`https://scrollscan.com/tx/${item.tx_hash}`}>
+                    {item.tx_hash.substring(0, 4) + '...' + item.tx_hash.substring(item.tx_hash.length - 4)}</Link>
+                </div>
+                <span className="text-center px-4 py-2">
                   {item.from_address.substring(0, 4) + '...' + item.from_address.substring(item.from_address.length - 4)}
-                </td>
-                <td className="border-t border-b border-gray-300 text-center px-4 py-2">
+                </span>
+                {/* <span className="text-center px-4 py-2">${item.quote_rate}</span> */}
+                {/* <span className="text-center px-4 py-2"> */}
+                {/*   {(parseFloat(item.pretty_quote.replace("$", "")) / item.quote_rate).toFixed(4)} {item.contract_ticker_symbol} */}
+                {/* </span> */}
+
+                <span className="text-center px-4 py-2">
                   {item.to_address.substring(0, 4) + '...' + item.to_address.substring(item.to_address.length - 4)}
-                </td>
-                <td className="border-t border-b border-gray-300 text-center px-4 py-2">
-                  {myWallet === item.from_address ? (
-                    <span className="bg-gray-400 rounded px-2 text-black p-1">{"In"}</span>
-                  ) : (
-                    <span className="bg-gray-400 rounded px-2 text-black p-1">{"Out"}</span>
-                  )}                            </td>
-                <td className="border-t border-b border-gray-300 text-center px-4 py-2">{item.pretty_value_quote}</td>
-              </tr>
+                </span>
+                <Badge className="w-max bg-gray-400">
+                  {myWallet === item.from_address ? 'IN' : 'OUT'}
+                </Badge>
+                <span className="text-center px-4 py-2">{item.pretty_value_quote}</span>
+              </>
             ))}
-            {!showAll &&
-              <div className="flex justify-center mt-4">
-                <button onClick={handleShowAll}
-                  className="mt-4 text-white font-bold text-center py-2 px-4rounded">View all txs</button>
-              </div>
-            }
-          </tbody>
-        </table>
-      </div>}
-    </div>
+          </div>
+          {!showAll &&
+            <div className="flex justify-center border-b border-r border-l rounded-br-lg border-gray-400 rounded-bl-lg">
+              <button onClick={handleShowAll}
+                className=" w-full text-white font-bold text-center py-2 px-4rounded hover:bg-gray-400/10  transition-colors">View all txs</button>
+            </div>
+          }
+        </>
+      }
+
+    </div >
 
 
   );
