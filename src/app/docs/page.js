67ff8c3fa4d32225146f4cd4e1328e5b48611bc3
hotwater <vlad.dalechin@gmail.com>
2024-04-27 16:23:12 +0300

    feat: set up shadcn

---
+++ src/app/docs/page.js
@@ -0,0 +1,15 @@
+
+const Docs = () => {
+  return (
+    <div>
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl">Docs Page</h1>
+        <p className="text-lg mt-4">
+          This is the Docs page content. You can add your documentation here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Docs;
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,15 @@
-import Navbar from '../app/components/NavBar';
 
 const Page = () => {
-    return (
-        <div className="font-custom">
-            <Navbar />
-            <div className="container mx-auto p-4">
-                <h1 className="text-4xl">Home, my Home</h1>
-                <p className="text-lg mt-4">
-                    This is the iiiiii page content. You can add your documentation here.
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="font-custom">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl">Home, my Home</h1>
+        <p className="text-lg mt-4">
+          This is the iiiiii page content. You can add your documentation here.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
